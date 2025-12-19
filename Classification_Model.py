711179c11a5d1705b347262ce3ae5a60ec07369e
Abdul <abdulkaish974@gmail.com>
2025-12-20 04:28:06 +0530

    This is Logistic Regression

---
+++ Classification_Model.py
@@ -0,0 +1,26 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+threshold = df['pollutant_avg'].mean()
+
+df['pollution_class'] = (df['pollutant_avg'] > threshold).astype(int)
+
+df[['pollutant_avg', 'pollution_class']].head()
+feature_cols = ['pollutant_min', 'pollutant_max', 'latitude', 'longitude',
+                'country_label', 'state_label', 'city_label',
+                'station_label', 'pollutant_id_label']
+
+X = df[feature_cols]
+y = df['pollution_class']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42
+)
+log_model = LogisticRegression(max_iter=1000)
+log_model.fit(X_train, y_train)
+
+y_pred_log = log_model.predict(X_test)
+
+print(" Logistic Regression Accuracy:", accuracy_score(y_test, y_pred_log))
+print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred_log))
+print("\nClassification Report:\n", classification_report(y_test, y_pred_log))
+
