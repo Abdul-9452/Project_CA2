1a449dccf0d2f625ace7c7497150c0ac46bcb4f1
Abdul <abdulkaish974@gmail.com>
2025-12-20 05:07:16 +0530

    final commit for Classification

--- Classification_Model.py
+++ Classification_Model.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -37,7 +36,7 @@ print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred_knn))
 print("\nClassification Report:\n", classification_report(y_test, y_pred_knn))
 
 
-=======
+
 from sklearn.naive_bayes import GaussianNB
 nb = GaussianNB()
 nb.fit(X_train, y_train)
@@ -47,4 +46,4 @@ y_pred_nb = nb.predict(X_test)
 print(" Naive Bayes Accuracy:", accuracy_score(y_test, y_pred_nb))
 print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred_nb))
 print("\nClassification Report:\n", classification_report(y_test, y_pred_nb))
->>>>>>> branch5
+
